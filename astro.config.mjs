cd5b78081dc8eb706edc4489a87469f8835e8d8b
shin870 <vamos.blumenkranz.628@gmail.com>
2026-02-15 21:32:22 +0900

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://nakataniplan.com',
+
+  output: 'static',
+
+  adapter: cloudflare(),
+
+  build: {
+    format: 'directory'
+  },
+
+  compressHTML: true
+});
+

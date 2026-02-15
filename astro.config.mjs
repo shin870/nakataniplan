1d24ab9be86bf38cefbd194c46352e59546aa053
shin870 <vamos.blumenkranz.628@gmail.com>
2026-02-15 21:39:02 +0900

    remove cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,6 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-  site: 'https://nakataniplan.com',
-
-  output: 'static',
-
-  adapter: cloudflare(),
-
-  build: {
-    format: 'directory'
-  },
-
-  compressHTML: true
+  site: 'https://nakataniplan.com'
 });
 
